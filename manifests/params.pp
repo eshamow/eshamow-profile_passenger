54699e5ccb9db2167447a5acd392dcf3e50cc645
Eric Shamow <eric.shamow@gmail.com>
2014-09-17 11:58:44 -0700

    Correct absolute fact reference

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class profile_passenger::params {
   $passenger_version = '4.0.50'
-  if $osfamily == 'debian' and $::operatingsystemmajrelease == '10.04' {
+  if $::osfamily == 'debian' and $::operatingsystemmajrelease == '10.04' {
     $bundler_enable = '1.6.6'
   } else {
     $bundler_enable = 'true'
