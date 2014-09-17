4825282e96ad23d5dbaa0ef34f3dd7ef4d169834
Eric Shamow <eric.shamow@gmail.com>
2014-09-17 13:05:30 -0700

    Fix bundler version for ubuntu 10

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class profile_passenger::params {
   $passenger_version = '4.0.50'
   if $::osfamily == 'debian' and $::operatingsystemmajrelease == '10.04' {
-    $bundler_ensure = '1.6.6'
+    $bundler_ensure = '0.9.9'
   } else {
     $bundler_ensure = 'installed'
   }
