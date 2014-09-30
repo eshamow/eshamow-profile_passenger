f2a8c13f35c38f99ff3262594ead3f5115892c25
Eric Shamow <eric.shamow@gmail.com>
2014-09-29 19:20:14 -0700

    Correct lint error & update gitignore

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,5 @@
+# == Class: profile_passenger::params
+#
 class profile_passenger::params {
   $passenger_version = '4.0.50'
   if $::osfamily == 'debian' and $::operatingsystemmajrelease == '10.04' {
