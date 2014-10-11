6ea744e7daeca75ddf321029e55c964ceb8e50ee
Eric Shamow <eric.shamow@gmail.com>
2014-10-11 13:25:58 -0700

    Get acceptance harness working and aligned with fixtures.yml and add all nodes to nodeset

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,23 +1,21 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 
+
 unless ENV['RS_PROVISION'] == 'no'
   # This will install the latest available package on el and deb based
   # systems fail on windows and osx, and install via gem on other *nixes
   foss_opts = { :default_action => 'gem_install' }
 
+  if default.is_pe?; then install_pe; else install_puppet( foss_opts ); end
+
   hosts.each do |host|
     if host['platform'] =~ /debian/
       on host, 'echo \'export PATH=/var/lib/gems/1.8/bin/:${PATH}\' >> ~/.bashrc'
-#    elsif host['platform'] =~ /redhat/
-    else
-      on host, 'yum -y install rubygems'
     end
+
     on host, "mkdir -p #{host['distmoduledir']}"
   end
-
-  if default.is_pe?; then install_pe; else install_puppet( foss_opts ); end
-
 end
 
 UNSUPPORTED_PLATFORMS = ['Suse','windows','AIX','Solaris']
@@ -32,15 +30,18 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'profile_passenger')
     hosts.each do |host|
+      copy_module_to(host, :source => proj_root, :module_name => 'profile_passenger')
+      on host, puppet('module','install','puppetlabs-stdlib','-v','4.1.0'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module','install','puppetlabs-apache','-v','1.1.1'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module','install','puppetlabs-concat','-v','1.1.1'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module','install','puppetlabs-gcc','-v','0.2.0'), { :acceptable_exit_codes => [0,1] }
       install_package host, 'git'
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'puppetlabs-apache'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'puppetlabs-concat'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'puppetlabs-gcc'), { :acceptable_exit_codes => [0,1] }
-      on host, shell('cd /etc/puppet/modules && git clone https://github.com/eshamow/puppetlabs-passenger.git passenger && cd /etc/puppet/modules/passenger && git checkout 270bf462a9942f5c534684bef2767cedb98707a0'), { :acceptable_exit_codes => [0,1] }
-      on host, shell('cd /etc/puppet/modules && git clone https://github.com/puppetlabs/puppetlabs-ruby.git ruby && cd /etc/puppet/modules/ruby && git checkout d8131b0535c3bb8bc2e72dca5d0b0e6088a65374'), { :acceptable_exit_codes => [0,1] }
+      on host, "git clone https://github.com/eshamow/puppetlabs-passenger.git /etc/puppet/modules/passenger"
+      on host, "cd /etc/puppet/modules/passenger && git checkout 09d181a2a818b177db106917c87327a0d8593dbb"
+      on host, "git clone https://github.com/puppetlabs/puppetlabs-ruby.git /etc/puppet/modules/ruby"
+      on host, "cd /etc/puppet/modules/ruby && git checkout 734a33bb7f7670136e77179aafeeebb81d8d14d3"
+
     end
   end
 end
